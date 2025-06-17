b4d929e0afb2ddb70a4952accf71170b3af86f96
katelapo <hubillahpunk@gmail.com>
2025-06-17 12:32:55 +0700

    frontend edit data

--- src/views/EditEshopView.vue
+++ src/views/EditEshopView.vue
@@ -1,28 +1,41 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
 const router = useRouter();
+
+const route = useRoute()
+const id = route.params.id
 
 const name = ref('')
 const place = ref('')
 const time = ref('')
 
 const saveData = async () => {
-    const newEshop = JSON.stringify({
+    const event = JSON.stringify({
         name: name.value,
         place: place.value,
         time: Date.parse(time.value)/1000,
     })
 
-    const response = await fetch('/api/eshops', {
-        method:'POST',
-        body: newEshop,
+    const response = await fetch(`/api/eshops/${id}`, {
+        method:'PUT',
+        body: event,
     })
     const data = await response.json()
 
     router.push('/')
 }
+
+onMounted(() => {
+    fetch(`/api/eshops/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            name.value = data.name
+            place.value = data.place
+            time.value = new Date(data.time * 1000).toISOString().slice(0, 16)
+        })
+})
 </script>
 
 <template>
